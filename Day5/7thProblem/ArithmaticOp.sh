9bd7cbb872f33d5213f03359e26dcd25ae58fb3d
sambit420 <sambit2024@gmail.com>
2022-09-19 00:51:18 +0530

    Added: Day5 Assignment

---
+++ Day5/7thProblem/ArithmaticOp.sh
@@ -0,0 +1,16 @@
+read -p "Enter a:-" a
+read -p "Enter b:-" b
+read -p "Enter c:-" c
+	
+	m=$((a+b*c))
+	n=$((a%b+c))
+	o=$((c+a/b))
+	p=$((a*b+c))
+max=0
+min=0
+if [ $m -ge $max ] && [ $m -ge $n ] && 
+then
+	max=$m
+
+
+
